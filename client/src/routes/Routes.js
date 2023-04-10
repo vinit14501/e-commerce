324c5e019c71f28d25f0dedd6f6da382b8a02aaf
vinit14501 <57012278+vinit14501@users.noreply.github.com>
2023-04-10 16:48:14 +0530

    second commit

---
+++ client/src/routes/Routes.js
@@ -0,0 +1,4 @@
+import LoginPage from '../pages/Login';
+import SignupPage from '../pages/Signup';
+
+export { LoginPage, SignupPage };
---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// const ErrorHandler = require('./middleware/error');
+
+const app = express();
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   })
+// );
+// app.use('/', express.static('uploads'));
+// app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+// CONFIG
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({
+    path: 'backend/config/.env',
+  });
+}
+
+// ROUTES
+// const user = require('./controller/user');
+// const shop = require('./controller/shop');
+// const product = require('./controller/product');
+// const event = require('./controller/event');
+// const coupon = require('./controller/coupounCode');
+
+// app.use('/api/v2/user', user);
+// app.use('/api/v2/shop', shop);
+// app.use('/api/v2/product', product);
+// app.use('/api/v2/event', event);
+// app.use('/api/v2/coupon', coupon);
+
+// FOR ERRORHANDLING
+// app.use(ErrorHandler);
+
+module.exports = app;
